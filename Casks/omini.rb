7aa8140f65abc51a165b95d95efca774d050fef7
manu-mishra <mishra.manu@outlook.com>
2026-04-07 23:17:53 -0500

    cask: omini 1.7.0

--- Casks/omini.rb
+++ Casks/omini.rb
@@ -1,6 +1,6 @@
 cask "omini" do
-  version "1.6.1"
-  sha256 "8d97d1621fcc4f4aee1fdcbf5702cb8bb43c5ed9b8918df900014f256665439c"
+  version "1.7.0"
+  sha256 "99058e5aa0b4418eb89fdf31f186df974e44c49ded63ff8dfa85dd92a0fdf2c2"
 
   url "https://ominidev.com/downloads/Omini-#{version}.dmg"
   name "Omini"
