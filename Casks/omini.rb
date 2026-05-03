b63707dc016ba0bf4190c703c65ba0bd381c4754
Manu Mishra <manu.masterman@gmail.com>
2026-05-02 21:07:38 -0500

    cask: omini 0.1.2

--- Casks/omini.rb
+++ Casks/omini.rb
@@ -1,6 +1,6 @@
 cask "omini" do
-  version "0.1.1"
-  sha256 "8c9f4a7ef9843878a98d70dce5da8e621e4f20369b070072516aa6c24d361e9a"
+  version "0.1.2"
+  sha256 "0221fcbac630382e1c73c15414f5fac56dd3a972acba3ca62b7fc8544a9fbe83"
 
   url "https://ominidev.com/downloads/OminiDev-#{version}.dmg"
   name "OminiDev"
